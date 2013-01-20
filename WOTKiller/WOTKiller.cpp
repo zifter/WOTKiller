bcc8a31de5b590ddc496dd2588499e2c4cf593d8
zifter <zifter.ai@gmail.com>
2013-01-20 21:16:41 +0300

    Init. View all running process.

---
+++ WOTKiller/WOTKiller.cpp
@@ -0,0 +1,82 @@
+// WOTKiller.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+#include <iostream>
+#include <windows.h>
+#include <stdio.h>
+#include <tchar.h>
+#include <psapi.h>
+
+// To ensure correct resolution of symbols, add Psapi.lib to TARGETLIBS
+// and compile with -DPSAPI_VERSION=1
+
+void PrintProcessNameAndID( DWORD processID )
+{
+	TCHAR szProcessName[MAX_PATH] = TEXT("<unknown>");
+
+	// Get a handle to the process.
+
+	HANDLE hProcess = OpenProcess( PROCESS_QUERY_INFORMATION |
+		PROCESS_VM_READ,
+		FALSE, processID );
+
+	// Get the process name.
+
+	if (NULL != hProcess )
+	{
+		HMODULE hMod;
+		DWORD cbNeeded;
+
+		if ( EnumProcessModules( hProcess, &hMod, sizeof(hMod), 
+			&cbNeeded) )
+		{
+			GetModuleBaseName( hProcess, hMod, szProcessName, 
+				sizeof(szProcessName)/sizeof(TCHAR) );
+		}
+	}
+
+	// Print the process name and identifier.
+	if(_tcscmp(szProcessName, TEXT("<unknown>")))
+	{
+		_tprintf( TEXT("%s  (PID: %u)\n"), szProcessName, processID );
+	}
+	
+
+	// Release the handle to the process.
+
+	CloseHandle( hProcess );
+}
+
+int main( void )
+{
+	// Get the list of process identifiers.
+
+	DWORD aProcesses[1024], cbNeeded, cProcesses;
+	unsigned int i;
+
+	if ( !EnumProcesses( aProcesses, sizeof(aProcesses), &cbNeeded ) )
+	{
+		return 1;
+	}
+
+
+	// Calculate how many process identifiers were returned.
+
+	cProcesses = cbNeeded / sizeof(DWORD);
+
+	// Print the name and process identifier for each process.
+
+	for ( i = 0; i < cProcesses; i++ )
+	{
+		if( aProcesses[i] != 0 )
+		{
+			PrintProcessNameAndID( aProcesses[i] );
+		}
+	}
+
+	int a;
+	std::cin >> a;
+	return 0;
+}
